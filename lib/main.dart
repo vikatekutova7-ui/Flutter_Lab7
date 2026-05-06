b61d4d059c4c860c1e18dbea9cb8b8fc6b2707c6
vika <vikatekutova7@gmail.com>
2026-05-06 14:54:51 +0300

    Update SoundService to use audioplayers for Web and Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:slot_machine/slot_machine.dart';
-void main() {
+import 'package:slot_machine/sound_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SoundService.init();
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
--- lib/slot_machine.dart
+++ lib/slot_machine.dart
@@ -39,6 +39,7 @@ class _SlotMachineState extends State<SlotMachine> {
 
   Future<void> _spin() async{
     if (_coins <= 0 || _isSpinning) return;
+    await SoundService.playClick();
     SoundService.playClick();
     setState(() {
       _isSpinning = true;
@@ -63,26 +64,31 @@ class _SlotMachineState extends State<SlotMachine> {
     );
 
     await Future.delayed(Duration(milliseconds: 300));
-    setState(() {
-      _isSpinning = false;
-      if (result1 == result2 && result2 == result3){
+    String newMessage;
+    int coinsChange;
+    if (result1 == result2 && result2 == result3){
         if (result1 == 'assets/images/seven.png'){
-          _coins += 10;
-          _message = 'Джекпот +10 монет ';
-          SoundService.playJackpot();
+          coinsChange = 10;
+          newMessage = 'Джекпот +10 монет ';
+          await SoundService.playJackpot();
         } else {
-          _coins += 3;
-          _message = 'ПОбеда +3 монеты';
-          SoundService.playWin();
+          coinsChange = 3;
+          newMessage = 'ПОбеда +3 монеты';
+          await SoundService.playWin();
         }
-      } else {
-        _coins -= 1;
-        _message = 'Попробуйте еще раз';
-        SoundService.playLose();
       }
-    });
+      else{
+        coinsChange = -1;
+        newMessage = 'Попробуй еще раз(((';
+        await SoundService.playLose();
 
-  }
+      }
+      setState(() {
+        _isSpinning = false;
+        _coins += coinsChange;
+        _message = newMessage;
+      });
+    }
   
   void _toggleMute(){
     SoundService.toggleMute();
